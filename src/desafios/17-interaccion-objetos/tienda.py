4dbf1c06b1c349e3c3b3f8a932727d071b46edc0
Carlos Soto <odoo.soporte@gmail.com>
2026-07-25 02:44:46 +0000

    wip(d17): bases

---
+++ src/desafios/17-interaccion-objetos/tienda.py
@@ -0,0 +1,126 @@
+from abc import ABC, abstractmethod
+from producto import Producto
+
+# Una clase abstracta es una clase que no puede ser instanciada
+# Es la plantilla para otras clases
+class Tienda(ABC):
+    def __init__(self, nombre, delivery):
+        self.__nombre = nombre
+        self.__delivery = delivery
+        self.lista_productos = []
+
+    def ingresar_producto(self, nombre, precio, stock = 0):
+
+        producto_nuevo = Producto(nombre, precio, stock)
+
+        i = self.buscar_producto(producto_nuevo)
+        if i is None:
+            self.lista_productos.append(producto_nuevo)
+        else:
+            self.lista_productos[i] = self.lista_productos[i] + producto_nuevo
+
+
+    def buscar_producto(self, buscar: Producto) -> int | None:
+
+        try:
+            pos = self.lista_productos.index(buscar)
+        except Exception:
+            pos = None
+
+        return pos
+
+    @abstractmethod
+    def listar_productos(self):
+        pass
+
+    @abstractmethod
+    def realizar_venta(self):
+        pass
+
+
+class Minimarket(Tienda):
+
+    def listar_productos(self):
+        pass
+
+    def realizar_venta(self):
+        pass
+
+class Restaurante(Tienda):
+    def listar_productos(self):
+            salida = ""
+            for producto in self.lista_productos:
+                salida += f"- {producto.nombre} | ${producto.precio}\n"
+            return salida
+
+    def realizar_venta(self, nombre: str, cantidad: int):
+
+        venta = Producto(nombre, 0, cantidad)
+        i = self.buscar_producto(venta)
+
+        if i is None:
+            return "Producto no existe"
+
+        return "Venta completada"
+
+
+class Supermercado(Tienda):
+    def listar_productos(self):
+        salida = ""
+        for producto in self.lista_productos:
+            linea = f"- {producto.nombre} | ${producto.precio} | Stock: {producto.stock}"
+            if producto.stock < 10:
+                linea += " | Pocos productos disponibles"
+            salida += linea + "\n"
+        return salida
+
+    def realizar_venta(self, nombre: str, cantidad: int):
+
+        venta = Producto(nombre, 0, cantidad)
+        i = self.buscar_producto(venta)
+
+        if i is None:
+            return "Producto no existe"
+        elif self.lista_productos[i].stock == 0:
+            return "No es posible vender 0 productos o productos negativos."
+        elif self.lista_productos[i].stock < cantidad:
+            aux = self.lista_productos[i].stock
+            self.lista_productos[i].stock = 0
+            return f"Venta completada. Solo se vendieron {aux} unidades"
+
+        self.lista_productos[i] = self.lista_productos[i] - venta
+        return "Venta completada"
+
+
+class Farmacia(Tienda):
+
+    def listar_productos(self):
+        salida = ""
+        for producto in self.lista_productos:
+            linea = f"- {producto.nombre} | ${producto.precio}"
+            if producto.precio > 15_000:
+                linea += " | Envío gratis al solicitar este producto"
+            salida += linea + "\n"
+        return salida
+    def realizar_venta(self):
+        nombre = input("Producto a vender: ")
+        cantidad = int(input("Cantidad: "))
+
+        for incide, producto in enumerate(self.lista_productos):
+            if producto.nombre == nombre:
+
+                if cantidad > 3:
+                    print("En farmacia no se pueden vender más de 3 unidades.")
+                    break
+
+                if cantidad <= 0:
+                    print("No es posible vender 0 productos o produgtos negativos.")
+                    break
+
+                if producto.stock == 0:
+                    print("No hay stock.")
+                    break
+
+                self.lista_productos[incide] = producto - cantidad
+                return self.lista_productos
+        print("Producto no encontrado")
