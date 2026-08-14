12b80659eb8cd7e952ace97808c428fdec73d377
Carlos Soto <odoo.soporte@gmail.com>
2026-08-14 23:52:04 +0000

    wip(django-inicio): urls fundamentos

--- django_inicio/app/urls.py
+++ django_inicio/app/urls.py
@@ -15,10 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.db.migrations import serializer
 from django.urls import path
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+def get_cursos():
+    return [
+        {"id": 1, "nombre": "Python"},
+        {"id": 2, "nombre": "SQL"},
+        {"id": 3, "nombre": "Django"},
+    ]
 
 def hello(request):
+    # Podemos escribir directamente en la terminal
+    # del servidor
+    print('Hola mundo desde la terminal')
     return HttpResponse("Hello, world!")
 
 def inicio_html(request):
@@ -31,9 +42,33 @@ def inicio_html(request):
 
     return HttpResponse(algo)
 
+def listado_cursos(request):
+    cursos = get_cursos()
+    print(cursos)
 
+    algo = '<h1>Listado de cursos</h1>'
+    algo += '<ul>'
+
+    for curso in cursos:
+        algo += f'<li>{curso["id"]} - {curso["nombre"]}</li>'
+        print()
+
+    algo += '</ul>'
+
+    # Retornando un texto
+    # return HttpResponse("Listado de cursos")
+
+    # Retornar un json
+    #return JsonResponse(cursos, safe=False)
+
+    # Retornando HTML
+    return HttpResponse(algo)
+
+
+# [TODO] crear grupos de path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', hello),
     path('inicio/', inicio_html),
+    path('cursos/', listado_cursos),
 ]
