89f64c27765d6447e2ff5cd534deba817d52c19a
Carlos Soto <odoo.soporte@gmail.com>
2026-08-14 00:34:09 +0000

    add: el problema

---
+++ src/p30-el-problema.py
@@ -0,0 +1,22 @@
+cursos = [
+    {"id": 1, "nombre": "Python"},
+    {"id": 2, "nombre": "SQL"},
+    {"id": 3, "nombre": "Django"},
+]
+
+def main():
+    for curso in cursos:
+        print(curso["nombre"])
+
+if __name__ == "__main__":
+    main()
+
+
+
+# Flujo de trabajo hasta hoy
+# usuario -> terminal -> python -> procesa algo
+
+
+# Flujo de aplicación web
+# usuario -> navegador -> servidor web -> python -> respuesta -> navegador
+
