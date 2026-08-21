38e8f7f2008caea7281fe96ab6fd1b7342f7bbf4
Carlos Soto <odoo.soporte@gmail.com>
2026-08-21 01:01:01 +0000

    refactor: lista de cursos

--- django_inicio/cursos/views.py
+++ django_inicio/cursos/views.py
@@ -3,12 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse, JsonResponse
 
-def get_cursos():
-    return [
+cursos = [
         {"id": 1, "nombre": "Python", "uuid": "488b0a66-20f7-45ee-87ef-941675592dff"},
         {"id": 2, "nombre": "SQL", "uuid": "1a8cf5f4-20e1-4b38-b586-9d324f45c4a5"},
-        {"id": 3, "nombre": "Django", "uuid": "821eaf30-f6c6-486d-b120-32bdae88ab40"},
+        {"id": 3, "nombre": "Django 6.1", "uuid": "821eaf30-f6c6-486d-b120-32bdae88ab40"},
     ]
+
+def get_cursos():
+    return cursos
 
 def listado_cursos(request):
     cursos = get_cursos()
